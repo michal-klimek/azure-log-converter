9fbe558ac477daf6cba137e37987e1ce0da68195
Michał Klimek <michal.piotr.klimek@gmail.com>
2022-08-29 14:00:35 +0200

    (feature) setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='azure-log-converter',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/michal-klimek/azure-log-converter.git',
+    license='GPL-3.0',
+    author='Michał Klimek',
+    author_email='michal.piotr.klimek@gmail.com',
+    description='An utility tool that converts azure log files into a more readable form'
+)
